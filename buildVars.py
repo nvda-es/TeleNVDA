9ba48c4a1a01eea04657214ee9518ffa14c25704
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2021-12-05 21:41:57 +0100

    Updated BuildVars with the new add-on name

--- buildVars.py
+++ buildVars.py
@@ -11,27 +11,27 @@ addon_info = {
 	# for previously unpublished addons, please follow the community guidelines at:
 	# https://bitbucket.org/nvdaaddonteam/todo/raw/master/guidelines.txt
 	# add-on Name, internal for nvda
-	"addon_name" : "remote",
+	"addon_name" : "TeleNVDA",
 	# Add-on summary, usually the user visible name of the addon.
 	# Translators: Summary for this add-on to be shown on installation and add-on information.
-	"addon_summary" : _("Remote Support"),
+	"addon_summary" : _("Tele NVDA remote assistance"),
 	# Add-on description
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
-	"addon_description" : _("""Allows remote control of and remote access to another machine."""),
+	"addon_description" : _("""Allows remote control of and remote access to another machine. This add-on is based on NVDA Remote."""),
 	# version
-	"addon_version" : "2.5",
+	"addon_version" : "20211205-dev",
 	# Author(s)
-	"addon_author" : u"Tyler Spivey <tspivey@pcdesk.net>, Christopher Toth <q@q-continuum.net>",
+	"addon_author" : "Asociación Comunidad Hispanohablante de NVDA <contacto@nvda.es> and other contributors. Original work by Tyler Spivey <tspivey@pcdesk.net>, Christopher Toth <q@q-continuum.net>",
 	# URL for the add-on documentation support
-	"addon_url" : "http://NVDARemote.com",
+	"addon_url" : "https://github.com/nvda-es/TeleNVDA",
 	# Documentation file name
 	"addon_docFileName" : "readme.html",
 	# Minimum NVDA version supported (e.g. "2018.3.0", minor version is optional)
-	"addon_minimumNVDAVersion": "2019.3",
+	"addon_minimumNVDAVersion": "2019.3.0",
 	# Last NVDA version supported/tested (e.g. "2018.4.0", ideally more recent than minimum version)
-	"addon_lastTestedNVDAVersion": "2021.1",
+	"addon_lastTestedNVDAVersion": "2021.3.0",
 	# Add-on update channel (default is None, denoting stable releases, and for development releases, use "dev"; do not change unless you know what you are doing)
-	"addon_updateChannel" : None,
+	"addon_updateChannel" : "dev"
 }
 
 
@@ -40,6 +40,7 @@ import os.path
 # Define the python files that are the sources of your add-on.
 # You can use glob expressions here, they will be expanded.
 pythonSources = [
+	'addon/*.py',
 	'addon/globalPlugins/*/*.py',
 	'addon/globalPlugins/*/*.exe',
 	'addon/synthDrivers/*/*.py',
