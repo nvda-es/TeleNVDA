a5fc7b9a86dd3540a029be27c89a673f8f12f4bf
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2024-08-16 22:36:20 +0200

    Updated version to 2024.8.16

--- buildVars.py
+++ buildVars.py
@@ -19,7 +19,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description" : _("""Allows remote control of and remote access to another machine. This add-on is based on NVDA Remote."""),
 	# version
-	"addon_version" : "2024.8.10",
+	"addon_version" : "2024.8.16",
 	# Author(s)
 	"addon_author" : "Asociación Comunidad Hispanohablante de NVDA <contacto@nvda.es> and other contributors. Original work by Tyler Spivey <tspivey@pcdesk.net>, Christopher Toth <q@q-continuum.net>",
 	# URL for the add-on documentation support
