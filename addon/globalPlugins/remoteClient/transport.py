bcdd18bfea602a27b3efc22e65538fab992e009a
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2026-02-15 17:11:44 +0100

    Add ALPN protocol negotiation for socket connections. This will allow connecting via proxies which filter SSL traffic depending on the specified protocol. Eventually, this would allow to connect on port 443 if a Nginx server is properly configured to redirect this protocol to a NVDA Remote Server

--- addon/globalPlugins/remoteClient/transport.py
+++ addon/globalPlugins/remoteClient/transport.py
@@ -126,6 +126,7 @@ class TCPTransport(Transport):
 		server_sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
 		server_sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, 60000, 2000))
 		ctx = (ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT))
+		ctx.set_alpn_protocols(['nvdaremote/2.0'])
 		ctx.minimum_version = ssl.TLSVersion.TLSv1_2
 		if insecure:
 			ctx.check_hostname = False
