7020a594c20de0ba9f4db3691d9a63d7431fd9f2
José Manuel Delicado Alcolea <jmdaweb@hotmail.com>
2026-02-17 19:25:22 +0100

    Updated version to 2026.2.17

--- buildVars.py
+++ buildVars.py
@@ -14,7 +14,7 @@ addon_info = AddonInfo(
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	addon_description= _("""Allows remote control of and remote access to another machine. This add-on is based on NVDA Remote."""),
 	# version
-	addon_version= "2026.2.11",
+	addon_version= "2026.2.17",
 	# Author(s)
 	addon_author= "Asociación Comunidad Hispanohablante de NVDA <contacto@nvda.es> and other contributors. Original work by Tyler Spivey <tspivey@pcdesk.net>, Christopher Toth <q@q-continuum.net>",
 	# URL for the add-on documentation support
@@ -35,7 +35,7 @@ addon_info = AddonInfo(
 	addon_sourceURL= "https://github.com/nvda-es/TeleNVDA",
 	# Brief changelog for this version
 	# Translators: what's new content for the add-on version to be shown in the add-on store
-	addon_changelog=_("""Compatible with NVDA 2026.1."""),
+	addon_changelog=_("""Minor tweaks and bug fixes."""),
 )
 
 import os.path
--- changelog.md
+++ changelog.md
@@ -1,7 +1,8 @@
 This release brings the following changes:
 
-* Compatibility fixes for NVDA 2026.1.
+* When connecting, include the string "remote/2.0" in the ALPN protocols list.
+* When Windows goes to lock screen, control is immediately returned to the local machine. Unfortunately, this feature is only available in NVDA 2022.4 or greather.
 
-Important: some anti-virus software are flagging parts of this add-on as malicious. Specifically, `url_handler.exe`, which opens `remote://` and `tele://` links. If you don't use this feature, you can safely quarantine or delete the file. Otherwise, you must add it as an exception.
+Important: some anti-virus software may fla parts of this add-on as malicious. Specifically, `url_handler.exe`, which opens `remote://` and `tele://` links. If you don't use this feature, you can safely quarantine or delete the file. Otherwise, you must add it as an exception.
 
 SHA256: 
